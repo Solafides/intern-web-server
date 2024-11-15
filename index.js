06b9f71fc958187eae6ff58fbf40b71cebfdc33b
solomon <123379369+Solafides@users.noreply.github.com>
2024-11-15 19:04:48 +0300

    Update index.js w
    updating the previous localhost link with the vercel link when routes clicked

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app=express();
 const port=3000
 app.get("/",(req,res)=>{
-    res.send("<h2>hello friends</h2>  This is my simple web server which gives you some information about me<br> please check <a href='http://localhost:3000/name'>/name,</a><a href='http://localhost:3000/hobby'>/hobby,</a>and<a href='http://localhost:3000/dream'>/dream </a> routes to know about me")
+    res.send("<h2>hello friends</h2>  This is my simple web server which gives you some information about me<br> please check <a href='https://intern-web-server-i8hz.vercel.app/name'>/name,</a><a href='https://intern-web-server-i8hz.vercel.app/hobby'>/hobby,</a>and<a href='https://intern-web-server-i8hz.vercel.app/dream'>/dream </a> routes to know about me")
 })
 
 app.get("/name",(req,res)=>{
